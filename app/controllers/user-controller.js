702ef029c0c41400c540d9a3ea34d3b889a540ac
rayzhang-xingyunzh <ray.zhang@xingyunzh.com>
2016-09-12 17:58:46 +0800

    add console

--- app/controllers/user-controller.js
+++ app/controllers/user-controller.js
@@ -2,6 +2,7 @@ var uidHelper = require('../util/uidHelper');
 
 exports.loginByWechat = function(req,res){
 	uidHelper.loginByWechat(req.body.code,function(err,result){
+		console.log('uid result:',result);
 		res.send(result);
 	})
 }
