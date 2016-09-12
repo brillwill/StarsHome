9623ba1926ad4f52160bac2e1d10efe5d265e5f6
rayzhang-xingyunzh <ray.zhang@xingyunzh.com>
2016-09-12 15:18:57 +0800

    bug fix

--- app/routes/api/user-api.js
+++ app/routes/api/user-api.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-var userController = require("../controllers/user-controller.js");
+var userController = require("../../controllers/user-controller.js");
 
 
 router.post('/login/wechat', userController.loginByWechat);
